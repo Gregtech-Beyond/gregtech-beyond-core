6aa2fb1f08f3b8b9772887790439054c2f08742d
BLocky104 <farmermick13@web.de>
2024-04-20 14:31:12 +0200

    Platline Materials

--- src/main/java/gtb/api/unification/materials/GTBMaterials.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterials.java
@@ -1,27 +1,48 @@
 package gtb.api.unification.materials;
 import gregtech.api.unification.material.Material;
+import gtb.api.unification.GTBMaterialFlagAddition;
+import gtb.api.unification.materials.info.GTBElementMaterials;
 
 public class GTBMaterials {
+
     //Element Materials
 
     //FD Materials
+
     public static Material PlatinumConcentrate;
     public static Material PlatinumExtract;
     public static Material PalladiumExtract;
     public static Material PlatinumResidues;
     public static Material PotassiumDisulfate;
     public static Material LeachResidue;
-    public static Material RhodiumSulfateSolution;
     public static Material SodiumRuthenate;
-    public static Material RutheniumTetroxideSolution;
     public static Material RutheniumTetroxideLiquid;
     public static Material IridiumDioxide;
-    public static Material AcidicIridiumDioxide;
+    public static Material PlatinumSolution;
+    public static Material PlatinumSalt;
+    public static Material PalladiumMotherLiqour;
+    public static Material PalladiumSalt;
+    public static Material PalladiumMetallicPowder;
+
 
     //SD Materials
 
+    public static Material PalladiumEnrichedAmmonia;
+    public static Material AcidicIridiumDioxide;
+    public static Material ReprecipitatedPalladium;
+    public static Material RutheniumTetroxideSolution;
+    public static Material RhodiumSulfateSolution;
+
+
     //Unknown Materials
     public static Material PlatinumExtractionMixture;
+
+    //Organic Chemistry Materials
+
+    public static Material FormicAcid;
+    public static Material SodiumFormate;
+
+
 
     public static void init() {
         GTBMaterialPropertyAddition.init();
