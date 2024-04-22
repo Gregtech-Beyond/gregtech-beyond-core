45869bed96ca5f3fb51fad1f7f41b6e9b10fd222
BLocky104 <farmermick13@web.de>
2024-04-22 19:00:23 +0200

    add missing imports

--- src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
@@ -1,5 +1,9 @@
 package gtb.api.unification.materials;
 
+import static gregtech.api.GTValues.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.material.info.MaterialFlags.*;
+
 public class GTBMaterialPropertyAddition {
 
     public static void init() {}
