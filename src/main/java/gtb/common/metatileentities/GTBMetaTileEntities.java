0b312c6ab324ea7c11e913dad9765cbe8c702566
BLocky104 <farmermick13@web.de>
2024-05-04 18:39:52 +0200

    add Dehydrator to Branch

--- src/main/java/gtb/common/metatileentities/GTBMetaTileEntities.java
+++ src/main/java/gtb/common/metatileentities/GTBMetaTileEntities.java
@@ -1,7 +1,6 @@
 package gtb.common.metatileentities;
 
-import static gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity;
-import static gregtech.common.metatileentities.MetaTileEntities.registerSimpleMetaTileEntity;
+import static gregtech.common.metatileentities.MetaTileEntities.*;
 import static gtb.api.utils.GTBUtil.gtb;
 
 import gregtech.api.metatileentity.SimpleMachineMetaTileEntity;
@@ -13,6 +12,7 @@ import gtb.api.utils.GTBUtil;
 import gtb.common.metatileentities.multiblocks.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GTBMetaTileEntities {
 
@@ -29,27 +29,29 @@ public final class GTBMetaTileEntities {
     public static MetaTileEntityVacuumDistillationTower VACUUM_DISTILLATION_TOWER;
     public static MetaTileEntityDilutionTank DILUTION_TANK;
     public static MetaTileEntityFermentationVat FERMENTATION_VAT;
+    public static SimpleMachineMetaTileEntity[] DEHYDRATORS = new SimpleMachineMetaTileEntity[15];
+
+
 
     public static void init() {
         // Multiblocks
         WATER_TANK = registerMetaTileEntity(3000, new MetaTileEntityWaterTank(gtb("water_tank")));
-        SOLAR_THERMAL_CONCENTRATOR = registerMetaTileEntity(3001,
-                new MetaTileEntitySolarThermalConcentrator(gtb("solar_thermal_concentrator")));
+        SOLAR_THERMAL_CONCENTRATOR = registerMetaTileEntity(3001, new MetaTileEntitySolarThermalConcentrator(gtb("solar_thermal_concentrator")));
         BACTERIAL_VAT = registerMetaTileEntity(3002, new MetaTileEntityBacterialVat(gtb("bacterial_vat")));
         VACUUM_FURNACE = registerMetaTileEntity(3003, new MetaTileEntityVacuumFurnace(gtb("vacuum_furnace")));
-        ASTRO_MINING_STATION = registerMetaTileEntity(3004,
-                new MetaTileEntityAstroMiningStation(gtb("astro_mining_station")));
+        ASTRO_MINING_STATION = registerMetaTileEntity(3004, new MetaTileEntityAstroMiningStation(gtb("astro_mining_station")));
         BEDROCK_PUMP = registerMetaTileEntity(3005, new MetaTileEntityBedrockPump(gtb("bedrock_pump")));
-        BLACK_HOLE_COMPRESSOR = registerMetaTileEntity(3006,
-                new MetaTileEntityBlackHoleCompressor(gtb("black_hole_compressor")));
-        CATALYTIC_REFORMATION_UNIT = registerMetaTileEntity(3007,
-                new MetaTileEntityCatalyticReformationUnit(gtb("catalytic_reformation_unit")));
-        SOLID_FUEL_GENERATOR = registerMetaTileEntity(3008,
-                new MetaTileEntitySolidFuelGenerator(gtb("solid_fuel_generator")));
+        BLACK_HOLE_COMPRESSOR = registerMetaTileEntity(3006, new MetaTileEntityBlackHoleCompressor(gtb("black_hole_compressor")));
+        CATALYTIC_REFORMATION_UNIT = registerMetaTileEntity(3007, new MetaTileEntityCatalyticReformationUnit(gtb("catalytic_reformation_unit")));
+        SOLID_FUEL_GENERATOR = registerMetaTileEntity(3008, new MetaTileEntitySolidFuelGenerator(gtb("solid_fuel_generator")));
         COKER = registerMetaTileEntity(3009, new MetaTileEntityCoker(gtb("coker")));
-        VACUUM_DISTILLATION_TOWER = registerMetaTileEntity(3010,
-                new MetaTileEntityVacuumDistillationTower(gtb("vacuum_distillation_tower")));
+        VACUUM_DISTILLATION_TOWER = registerMetaTileEntity(3010, new MetaTileEntityVacuumDistillationTower(gtb("vacuum_distillation_tower")));
         DILUTION_TANK = registerMetaTileEntity(3011, new MetaTileEntityDilutionTank(gtb("dilution_tank")));
         FERMENTATION_VAT = registerMetaTileEntity(3012, new MetaTileEntityFermentationVat(gtb("fermentation_vat")));
+
+        registerSimpleMetaTileEntity(DEHYDRATORS, 3013, "dehydrator", GTBRecipeMaps.DEHYDRATOR,
+                GTBTextures.DEHYDRATOR_OVERLAY, true, GTBUtil::gtb, GTUtility.hvCappedTankSizeFunction);
+
     }
+
 }
