7f22fc245f507098e79f259e64cab450be335630
BLocky104 <farmermick13@web.de>
2024-05-15 21:29:40 +0200

    colorization of materials, also fixing some formulas

--- src/main/java/gtb/api/unification/materials/GTBFirstDegreeMaterials.java
+++ src/main/java/gtb/api/unification/materials/GTBFirstDegreeMaterials.java
@@ -151,324 +151,324 @@ public class GTBFirstDegreeMaterials {
 
         IsoamylAlcohol = new Material.Builder(getMaterialsId(), gregtechId("isoamyl_alcohol"))
                 .fluid()
-                .color(0xC87A7A)
+                .color(0x004D40)
                 .build().setFormula("C5H12O", true);
 
         IsobutylAlcohol = new Material.Builder(getMaterialsId(), gregtechId("isobutyl_alcohol"))
                 .fluid()
-                .color(0xAB47BC)
+                .color(0xF57F17)
                 .build().setFormula("C5H12O", true);
 
         Formaldehyde = new Material.Builder(getMaterialsId(), gregtechId("formaldehyde"))
                 .fluid()
-                .color(0xAB47BC)
+                .color(0x66BB6A)
                 .build().setFormula("CH20", true);
 
         Dicyclopentadiene = new Material.Builder(getMaterialsId(), gregtechId("dicyclopentadiene"))
                 .fluid()
-                .color(0xAB47BC)
+                .color(0xBA68C8)
                 .build().setFormula("C10H12", true);
 
         Cyclopentadiene = new Material.Builder(getMaterialsId(), gregtechId("cyclopentadiene"))
                 .fluid()
-                .color(0xAB47BC)
+                .color(0x625664)
                 .build().setFormula("C5H6", true);
 
         TungstenTrioxide = new Material.Builder(getMaterialsId(), gregtechId("tungstentrioxide"))
                 .dust()
-                .color(0x6E9C46)
+                .color(0xD9C572)
                 .build().setFormula("WO3", true);
 
         HydrogenPeroxide = new Material.Builder(getMaterialsId(), gregtechId("hydrogenperoxide"))
                 .fluid()
-                .color(0xC2E2F0)
+                .color(0x64B5F6)
                 .build().setFormula("H2O2", true);
 
         CalciumHydroxide = new Material.Builder(getMaterialsId(), gregtechId("calciumhydroxide"))
                 .dust()
-                .color(0x9FC0A1)
+                .color(0x1A237E)
                 .build().setFormula("Ca(OH)2", true);
 
         LithiumHydroxide = new Material.Builder(getMaterialsId(), gregtechId("lithiumhydroxide"))
                 .dust()
-                .color(0x1A4051)
+                .color(0xC4C8CB)
                 .build().setFormula("LiOH", true);
 
         SodiumTungstate = new Material.Builder(getMaterialsId(), gregtechId("sodiumtungstate"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x6A6A6A)
                 .build().setFormula("Na2WO4", true);
 
         CalciumTungstate = new Material.Builder(getMaterialsId(), gregtechId("calciumtungstate"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x667678)
                 .build().setFormula("WCaO4", true);
 
         LithiumHydroxideSolution = new Material.Builder(getMaterialsId(), gregtechId("lithiumhydroxidesolution"))
                 .dust()
                 .fluid()
-                .color(0x1A4051)
+                .color(0x0277BD)
                 .build().setFormula("(H2O)LiOH", true);
 
         LithiumFluoride = new Material.Builder(getMaterialsId(), gregtechId("lithiumfluoride"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x00796B)
                 .build().setFormula("(H2O)LiOH", true);
 
         NaquadahDioxide = new Material.Builder(getMaterialsId(), gregtechId("naquadah_dioxide"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x6A1B9A)
                 .build().setFormula("NqO2", true);
 
         Nylon = new Material.Builder(getMaterialsId(), gregtechId("nylon"))
                 .dust()
                 .fluid()
-                .color(0x1A4051)
+                .color(0xCAB788)
                 .build().setFormula("NqO2", true);
 
         ElectronDegenerateRhenium = new Material.Builder(getMaterialsId(), gregtechId("electron_degenerate_rhenium"))
                 .dust()
-                .color(0x1A4051)
+                .color(0xDCD4EA)
                 .build().setFormula("NqO2", true);
 
         ElectronDegenerateRheniumPlasma = new Material.Builder(getMaterialsId(),
                 gregtechId("electron_degenerate_rhenium_plasma"))
                         .fluid()
-                        .color(0x1A4051)
+                        .color(0x7986CB)
                         .build().setFormula("NqO2", true);
 
         HighPurityArsenic = new Material.Builder(getMaterialsId(), gregtechId("high_purity_arsenic"))
                 .dust()
-                .color(0xB0B0B1)
+                .color(0xD7CCC8)
                 .build().setFormula("NqO2", true);
 
         IndiumChloride = new Material.Builder(getMaterialsId(), gregtechId("indium_chloride"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x8E24AA)
                 .build().setFormula("NqO2", true);
 
         NovolacsPhotoresist = new Material.Builder(getMaterialsId(), gregtechId("novolacs_photoresist"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xAF877B)
                 .build().setFormula("NqO2", true);
 
         WhitePhosphorus = new Material.Builder(getMaterialsId(), gregtechId("white_phosphorus"))
                 .dust()
                 .fluid()
-                .color(0x1A4051)
+                .color(0xE6D9A0)
                 .build().setFormula("P4", true);
 
         LiquidXenon = new Material.Builder(getMaterialsId(), gregtechId("liquid_xenon"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x29B6F6)
                 .build().setFormula("NqO2", true);
 
         SiliconCarbide = new Material.Builder(getMaterialsId(), gregtechId("silicon_carbide"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x43535A)
                 .build().setFormula("NqO2", true);
 
         SiliconCarbideVapor = new Material.Builder(getMaterialsId(), gregtechId("silicon_carbide_vapor"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x5A707A)
                 .build().setFormula("NqO2", true);
 
         VeryHotNitrogen = new Material.Builder(getMaterialsId(), gregtechId("very_hot_nitrogen"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x73CBF2)
                 .build().setFormula("N", true);
 
         PhotopolymerSolution = new Material.Builder(getMaterialsId(), gregtechId("photopolymer_solution"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x4DB6AC)
                 .build().setFormula("NqO2", true);
 
         UsedPhotopolymerSolution = new Material.Builder(getMaterialsId(), gregtechId("used_photopolymer_solution"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xCE93D8)
                 .build().setFormula("NqO2", true);
 
         VeryHotXenon = new Material.Builder(getMaterialsId(), gregtechId("very_hot_xenon"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xAED5F5)
                 .build().setFormula("Xe", true);
 
         VeryHotArgon = new Material.Builder(getMaterialsId(), gregtechId("very_hot_argon"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x7CB342)
                 .build().setFormula("Ar", true);
 
         NickelNitride = new Material.Builder(getMaterialsId(), gregtechId("nickel_nitride"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xFFF176)
                 .build().setFormula("Ar", true);
 
         LiquidHelium = new Material.Builder(getMaterialsId(), gregtechId("liquid_helium"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xFDF8CC)
                 .build().setFormula("He", true);
 
         MetastableOganesson = new Material.Builder(getMaterialsId(), gregtechId("metastable_oganesson"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("He", true);
+                .color(0xB71C1C)
+                .build().setFormula("Og", true);
 
         VeryHotKrypton = new Material.Builder(getMaterialsId(), gregtechId("very_hot_krypton"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x81C784)
                 .build().setFormula("Kr", true);
 
         SuperFluidHelium_4 = new Material.Builder(getMaterialsId(), gregtechId("super_fluid_helium_4"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xFDD835)
                 .build().setFormula("He", true);
 
         Helium_4 = new Material.Builder(getMaterialsId(), gregtechId("helium_4"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x928550)
                 .build().setFormula("He", true);
 
         LiquidEnrichedHelium_4 = new Material.Builder(getMaterialsId(), gregtechId("liquid_enriched_helium_4"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xC0CA33)
                 .build().setFormula("He", true);
 
         Trichloroflerane = new Material.Builder(getMaterialsId(), gregtechId("trichloroflerane"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("He", true);
+                .color(0x6C3090)
+                .build().setFormula("Cl3HFl", true);
 
         NaquadahRichSterileGrowthMedium = new Material.Builder(getMaterialsId(),
                 gregtechId("naquadah_rich_sterile_growth_medium"))
                         .fluid()
-                        .color(0x1A4051)
-                        .build().setFormula("He", true);
+                        .color(0x272529)
+                        .build();
 
         HighDensityPolyethylene = new Material.Builder(getMaterialsId(), gregtechId("high_density_polyethylene"))
                 .fluid()
                 .dust()
-                .color(0x1A4051)
-                .build().setFormula("He", true);
+                .color(0xC8C8C8)
+                .build().setFormula("C2H4", true);
 
         LiquidNitrogen = new Material.Builder(getMaterialsId(), gregtechId("liquid_nitrogen"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xA5C8DA)
                 .build().setFormula("N", true);
 
         GraphiteOxide = new Material.Builder(getMaterialsId(), gregtechId("graphite_oxide"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x707272)
                 .build().setFormula("C(O2)", true);
 
         GrapheneOxide = new Material.Builder(getMaterialsId(), gregtechId("graphene_oxide"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x5C5D5D)
                 .build().setFormula("C(O2)", true);
 
         GrapheneOxidationResidues = new Material.Builder(getMaterialsId(), gregtechId("graphene_oxidation_residues"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x827717)
                 .build().setFormula("C(O2)", true);
 
         GrapheneOxidationSolution = new Material.Builder(getMaterialsId(), gregtechId("graphene_oxidation_solution"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xA29A56)
                 .build().setFormula("C(O2)", true);
 
         SilverBromide = new Material.Builder(getMaterialsId(), gregtechId("silver_bromides"))
                 .dust()
-                .color(0x1A4051)
-                .build().setFormula("C(O2)", true);
+                .color(0xD19E9E)
+                .build().setFormula("AgBr", true);
 
         Silane = new Material.Builder(getMaterialsId(), gregtechId("silane"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xA9A8A8)
                 .build().setFormula("SiCl4", true);
 
         VeryHotAquaRegia = new Material.Builder(getMaterialsId(), gregtechId("very_hot_aqua_regia"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("SiCl4", true);
+                .color(0xF9A825)
+                .build().setFormula("(HNO3)(HCL)2", true);
 
         ChloronaquadicAcid = new Material.Builder(getMaterialsId(), gregtechId("chloronaquadic_acid"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0x616161)
                 .build().setFormula("SiCl4", true);
 
         SodiumNitrate = new Material.Builder(getMaterialsId(), gregtechId("sodium_nitrate"))
                 .dust()
-                .color(0x1A4051)
-                .build().setFormula("SiCl4", true);
+                .color(0x65899A)
+                .build().setFormula("NaN2", true);
 
         BariumTitanate = new Material.Builder(getMaterialsId(), gregtechId("barium_titanate"))
                 .dust()
-                .color(0x1A4051)
-                .build().setFormula("SiCl4", true);
+                .color(0x88B68A)
+                .build().setFormula("BaTi02", true);
 
         HydrogenChloride = new Material.Builder(getMaterialsId(), gregtechId("hydrogen_chloride"))
                 .fluid()
-                .color(0x1A4051)
+                .color(0xC5CAE9)
                 .build().setFormula("HCl", true);
 
         DopedNanotubeDepositionMix = new Material.Builder(getMaterialsId(), gregtechId("doped_nanotube_deposition_mix"))
                 .dust()
-                .color(0x1A4051)
+                .color(0xFF9800)
                 .build().setFormula("HCl", true);
 
         NanotubeDepositionMix = new Material.Builder(getMaterialsId(), gregtechId("nanotube_deposition_mix"))
                 .dust()
-                .color(0x1A4051)
+                .color(0xB71C1C)
                 .build().setFormula("HCl", true);
 
         DopedNanotubeDepositionMixture = new Material.Builder(getMaterialsId(),
                 gregtechId("doped_nanotube_deposition_mixture"))
                         .dust()
-                        .color(0x1A4051)
+                        .color(0x1B5E20)
                         .build().setFormula("HCl", true);
 
         CarbonNanotubes = new Material.Builder(getMaterialsId(), gregtechId("carbon_nanotubes"))
                 .dust()
-                .color(0x1A4051)
+                .color(0x1A1A1A)
                 .build().setFormula("HCl", true);
 
         LiquidCrystalDetector = new Material.Builder(getMaterialsId(), gregtechId("liquid_crystal_detector"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("HCl", true);
+                .color(0x9C27B0)
+                .build().setFormula("?", true);
 
         FullerenePolymerMatrix = new Material.Builder(getMaterialsId(), gregtechId("fullerene_polymer_matrix"))
                 .fluid()
                 .dust()
-                .color(0x1A4051)
+                .color(0x313031)
                 .build().setFormula("HCl", true);
 
         FullerenePolymerTetrix = new Material.Builder(getMaterialsId(), gregtechId("fullerene_polymer_tetrix"))
                 .fluid()
                 .dust()
-                .color(0x1A4051)
+                .color(0x1D181E)
                 .build().setFormula("HCl", true);
 
         SeaborgiumDopedNanotubes = new Material.Builder(getMaterialsId(), gregtechId("seaborgium_doped_nanotubes"))
                 .fluid()
                 .dust()
-                .color(0x1A4051)
+                .color(0x0D47A1)
                 .build().setFormula("HCl", true);
 
         NaquadriaticTaranium = new Material.Builder(getMaterialsId(), gregtechId("naquadadriatic_taranium"))
                 .fluid()
                 .dust()
-                .color(0x1A4051)
-                .build().setFormula("HCl", true);
+                .color(0x4A4B4B)
+                .build().setFormula("NqTr", true);
 
         ExtraDistilledWater = new Material.Builder(getMaterialsId(), gregtechId("extra_distilled_water"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("HCl", true);
+                .color(0x265399)
+                .build().setFormula("H20", true);
 
         Trichlorosilane = new Material.Builder(getMaterialsId(), gregtechId("trichlorosilane"))
                 .fluid()
-                .color(0x1A4051)
-                .build().setFormula("HCl", true);
+                .color(0x444545)
+                .build().setFormula("HCl4", true);
     }
 
     private static int getMaterialsId() {
