6a4fa8297ea077e240a616b69d61a1b048ca2719
jude123412 <104434396+jude123412@users.noreply.github.com>
2024-05-15 11:35:20 +0100

    Tungsten and Damascus Steel Line
    Added Damascus Steel Line
    Fixed Tungsten Line Recipe
    Added files for all items that don't have them
    Added Damascus Steel Line Meta Items

--- src/main/java/gtb/common/item/GTBMetaItemRegistry.java
+++ src/main/java/gtb/common/item/GTBMetaItemRegistry.java
@@ -13,6 +13,14 @@ public class GTBMetaItemRegistry extends StandardMetaItem {
         GTBMetaItems.ASTRO_MINER = addItem(1, "astro_miner");
         GTBMetaItems.MOON = addItem(2, "moon");
         GTBMetaItems.PARAFFIN_WAX = addItem(3, "paraffin_wax");
-        GTBMetaItems.BITOMINOUS_RESIDUES = addItem(4, "bitominous_residues");
+        GTBMetaItems.BITUMINOUS_RESIDUES = addItem(4, "bituminous_residues");
+        GTBMetaItems.DAMASCUS_STEEL_ASSEMBLY = addItem(5, "damascus_steel_assembly");
+        GTBMetaItems.HOT_DAMASCUS_STEEL_ASSEMBLY = addItem(6, "hot_damascus_steel_assembly");
+        GTBMetaItems.LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(7, "lightly_forged_damascus_steel_assembly");
+        GTBMetaItems.HOT_LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(8, "hot_lightly_forged_damascus_steel_assembly");
+        GTBMetaItems.MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(9, "moderately_forged_damascus_steel_assembly");
+        GTBMetaItems.HOT_MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(10, "hot_moderately_forged_damascus_steel_assembly");
+        GTBMetaItems.HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(11, "highly_forged_damascus_steel_assembly");
+        GTBMetaItems.HOT_HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY = addItem(12, "hot_highly_forged_damascus_steel_assembly");
     }
 }
---
+++ src/main/java/gtb/loaders/recipe/DamascusSteelLine.java
@@ -0,0 +1,80 @@
+package gtb.loaders.recipe;
+
+import gtb.common.item.GTBMetaItems;
+
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+
+
+public class DamascusSteelLine {
+
+    public static void init() {
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .input(plate, Steel, 8)
+                .input(plate, BlackSteel, 8)
+                .input(dust, Borax, 8)
+                .circuitMeta(16)
+                .fluidInputs(Steel.getFluid(576))
+                .output(GTBMetaItems.DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(300)
+                .EUt(512)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.HOT_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(1200)
+                .EUt(1920)
+                .buildAndRegister();
+
+        FORGE_HAMMER_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.HOT_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(160)
+                .EUt(384)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.HOT_LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(1200)
+                .EUt(1920)
+                .buildAndRegister();
+
+        FORGE_HAMMER_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.HOT_LIGHTLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(160)
+                .EUt(384)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.HOT_MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(1200)
+                .EUt(1920)
+                .buildAndRegister();
+
+        FORGE_HAMMER_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.HOT_MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(160)
+                .EUt(384)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(GTBMetaItems.HOT_HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .duration(1200)
+                .EUt(1920)
+                .buildAndRegister();
+
+        FORGE_HAMMER_RECIPES.recipeBuilder()
+                .input(GTBMetaItems.HOT_HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY, 1)
+                .output(plate, DamascusSteel, 1)
+                .duration(160)
+                .EUt(384)
+                .buildAndRegister();
+    }
+}
