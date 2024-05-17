178975ec8ace4273022a34705e33d0ae3fc96252
BLocky104 <farmermick13@web.de>
2024-05-18 00:56:26 +0200

    add Fluid Property to Phophorus Pentoxide

--- src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
@@ -1,6 +1,7 @@
 package gtb.api.unification.materials;
 
 import static gregtech.api.unification.material.Materials.Bromine;
+import static gregtech.api.unification.material.Materials.PhosphorusPentoxide;
 
 import gregtech.api.unification.material.properties.FluidProperty;
 import gregtech.api.unification.material.properties.PropertyKey;
@@ -9,5 +10,6 @@ public class GTBMaterialPropertyAddition {
 
     public static void init() {
         Bromine.setProperty(PropertyKey.FLUID, new FluidProperty());
+        PhosphorusPentoxide.setProperty(PropertyKey.FLUID, new FluidProperty());
     }
 }
