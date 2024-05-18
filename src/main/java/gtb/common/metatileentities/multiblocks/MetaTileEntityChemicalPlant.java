1359c38263b7f706bd8d70cd36266bc2686c881a
BLocky104 <farmermick13@web.de>
2024-05-18 22:46:59 +0200

    add Chemical Plant

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityChemicalPlant.java
@@ -0,0 +1,97 @@
+package gtb.common.metatileentities.multiblocks;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockBoilerCasing;
+import gregtech.common.blocks.MetaBlocks;
+
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
+import gtb.api.recipes.GTBRecipeMaps;
+import gtb.common.block.GTBMetaBlocks;
+import gtb.common.block.blocks.GTBMultiblockCasing;
+
+public class MetaTileEntityChemicalPlant extends RecipeMapMultiblockController {
+
+    public MetaTileEntityChemicalPlant(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.CHEMICAL_PLANT);
+        initializeAbilities();
+    }
+
+    public IBlockState getCasingState() {
+        return GTBMetaBlocks.GTB_MULTIBLOCK_CASING
+                .getState(GTBMultiblockCasing.CasingType.FULLERENE_POLYMER_TETRIX_CASING);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("X~~~X", "XXXXX", "X~~~X", "XXXXX", "X~~~X")
+                .aisle("XXXXX", "XHHHX", "XPPPX", "XCCCX", "XXXXX")
+                .aisle("X~~~X", "XPPPX", "X~~~X", "XPPPX", "X~~~X")
+                .aisle("XXXXX", "XHHHX", "XPPPX", "XCCCX", "XXXXX")
+                .aisle("X~~~X", "SXXXX", "X~~~X", "XXXXX", "X~~~X")
+                .where('S', selfPredicate())
+                .where('~', any())
+                .where('C', states(getCasingState())
+                        .or(abilities(MultiblockAbility.EXPORT_FLUIDS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.IMPORT_ITEMS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.INPUT_ENERGY).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.IMPORT_FLUIDS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.EXPORT_ITEMS).setExactLimit(1)))
+                .where('H', frames(Materials.Steel))
+                .where('H', heatingCoils())
+                .where('P',
+                        states(MetaBlocks.BOILER_CASING
+                                .getState(BlockBoilerCasing.BoilerCasingType.POLYTETRAFLUOROETHYLENE_PIPE)))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(false, false, true, false, false, true, false);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.INERT_PTFE_CASING;
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        getFrontOverlay().renderOrientedState(renderState, translation, pipeline, getFrontFacing(),
+                recipeMapWorkable.isActive(), recipeMapWorkable.isWorkingEnabled());
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.ASSEMBLER_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityChemicalPlant(metaTileEntityId);
+    }
+}
