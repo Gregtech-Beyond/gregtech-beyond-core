ae2c2e43d89b17226ed1dbb481afe147fdc2f90f
BLocky104 <farmermick13@web.de>
2024-05-20 17:57:02 +0200

    add Fluid Property to Sodium Sulfide

--- src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
@@ -10,14 +10,15 @@ public class GTBMaterialPropertyAddition {
     public static void init() {
         Bromine.setProperty(PropertyKey.FLUID, new FluidProperty());
         PhosphorusPentoxide.setProperty(PropertyKey.FLUID, new FluidProperty());
+        SodiumSulfide.setProperty(PropertyKey.FLUID, new FluidProperty());
         IndiumPhosphide.setProperty(PropertyKey.INGOT, new IngotProperty());
         Moscovium.setProperty(PropertyKey.DUST, new DustProperty());
         Nihonium.setProperty(PropertyKey.DUST, new DustProperty());
         Thallium.setProperty(PropertyKey.DUST, new DustProperty());
+        Iodine.setProperty(PropertyKey.DUST, new DustProperty());
         NaquadahAlloy.addFlags(MaterialFlags.GENERATE_FINE_WIRE);
         Graphene.addFlags(MaterialFlags.GENERATE_FOIL);
         Mica.addFlags(MaterialFlags.GENERATE_FOIL);
         BorosilicateGlass.addFlags(MaterialFlags.GENERATE_PLATE);
-
     }
 }
