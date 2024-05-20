35d36f4fea94f9ed5474382b04d70a992c257a7e
BLocky104 <farmermick13@web.de>
2024-05-20 18:45:41 +0200

    add recipe for NaK coolant

--- src/main/java/gtb/loaders/recipe/Metallurgy.java
+++ src/main/java/gtb/loaders/recipe/Metallurgy.java
@@ -3,6 +3,11 @@ package gtb.loaders.recipe;
 import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.*;
+import static gregtech.common.items.MetaItems.*;
+import static gtb.common.item.GTBMetaItems.*;
+
+import gregtech.api.recipes.GTRecipeHandler;
+import gregtech.api.unification.OreDictUnifier;
 
 public final class Metallurgy {
 
@@ -14,5 +19,23 @@ public final class Metallurgy {
                 .duration(122)
                 .EUt(24)
                 .buildAndRegister();
+
+        BENDER_RECIPES.recipeBuilder()
+                .input(FLUID_CELL)
+                .output(COOLANT_CELL)
+                .circuitMeta(1)
+                .duration(1200)
+                .EUt(8)
+                .buildAndRegister();
+
+        CANNER_RECIPES.recipeBuilder()
+                .fluidInputs(SodiumPotassium.getFluid(1000))
+                .input(COOLANT_CELL)
+                .output(NAK_COOLANT_CELL)
+                .duration(120)
+                .EUt(12)
+                .buildAndRegister();
+
+
     }
 }
