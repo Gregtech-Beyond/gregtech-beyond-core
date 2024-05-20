680cde9ea4f7f243b0aab279f9af194a145b615d
BLocky104 <farmermick13@web.de>
2024-05-20 20:24:56 +0200

    start making recipes for Machines

--- src/main/java/gtb/loaders/recipe/MachineRecipes.java
+++ src/main/java/gtb/loaders/recipe/MachineRecipes.java
@@ -8,7 +8,6 @@ import static gregtech.loaders.recipe.CraftingComponent.*;
 import static gregtech.loaders.recipe.MetaTileEntityLoader.*;
 import static gtb.api.unification.materials.GTBMaterials.*;
 
-import gregtech.api.recipes.GTRecipeHandler;
 import gregtech.api.recipes.ModHandler;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.MarkerMaterials;
@@ -16,28 +15,23 @@ import gregtech.common.blocks.BlockBoilerCasing;
 import gregtech.common.blocks.BlockMachineCasing;
 import gregtech.common.blocks.BlockMetalCasing;
 import gregtech.common.blocks.MetaBlocks;
-import gregtech.common.items.MetaItems;
 import gregtech.common.metatileentities.MetaTileEntities;
 import gregtech.common.metatileentities.storage.*;
-import gtb.api.unification.materials.GTBMaterials;
-import gtb.common.block.GTBMetaBlocks;
-import gtb.common.item.GTBMetaItems;
+
 import gtb.common.metatileentities.GTBMetaTileEntities;
-import net.minecraft.block.BlockWorkbench;
 
 public class MachineRecipes {
 
     public static void init() {
-
-    ASSEMBLER_RECIPES.recipeBuilder()
-            .input(plate, StainlessSteel, 4)
-            .input(circuit, MarkerMaterials.Tier.EV, 2)
-            .input(ELECTRIC_MOTOR_IV, 4)
-            .input(ELECTRIC_PUMP_IV, 4)
-            .outputs(GTBMetaTileEntities.BACTERIAL_VAT.getStackForm(1))
-            .duration(1200)
-            .EUt(800)
-            .buildAndRegister();
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .input(plate, StainlessSteel, 4)
+                .input(circuit, MarkerMaterials.Tier.EV, 2)
+                .input(ELECTRIC_MOTOR_IV, 4)
+                .input(ELECTRIC_PUMP_IV, 4)
+                .outputs(GTBMetaTileEntities.BACTERIAL_VAT.getStackForm(1))
+                .duration(1200)
+                .EUt(800)
+                .buildAndRegister();
 
         ASSEMBLER_RECIPES.recipeBuilder()
                 .input(MetaTileEntities.STEEL_TANK.getStackForm(1).getItem())
@@ -49,19 +43,21 @@ public class MachineRecipes {
                 .EUt(800)
                 .buildAndRegister();
 
-
         ASSEMBLY_LINE_RECIPES.recipeBuilder()
                 .input(foil, Polybenzimidazole, 32)
                 .input(plate, Grisium, 32)
                 .input(stickLong, Cupronickel, 32)
                 .input(pipeLargeFluid, Polytetrafluoroethylene, 8)
-                .input(MetaBlocks.BOILER_CASING.getItemVariant(BlockBoilerCasing.BoilerCasingType.POLYTETRAFLUOROETHYLENE_PIPE).getItem(), 4)
-                .input(MetaBlocks.METAL_CASING.getItemVariant(BlockMetalCasing.MetalCasingType.PTFE_INERT_CASING).getItem(), 4)
+                .inputs(MetaBlocks.BOILER_CASING
+                        .getItemVariant(BlockBoilerCasing.BoilerCasingType.POLYTETRAFLUOROETHYLENE_PIPE))
+                .inputs(MetaBlocks.METAL_CASING.getItemVariant(BlockMetalCasing.MetalCasingType.PTFE_INERT_CASING))
                 .input(circuit, MarkerMaterials.Tier.IV, 4)
                 .input(ELECTRIC_MOTOR_IV, 4)
                 .fluidInputs(Polytetrafluoroethylene.getFluid(2304))
                 .fluidInputs(SolderingAlloy.getFluid(2304))
                 .output(GTBMetaTileEntities.CHEMICAL_PLANT)
+                .duration(1200)
+                .EUt(800)
                 .buildAndRegister();
 
         ASSEMBLY_LINE_RECIPES.recipeBuilder()
@@ -69,59 +65,38 @@ public class MachineRecipes {
                 .fluidInputs(SolderingAlloy.getFluid(2304))
                 .input(circuit, MarkerMaterials.Tier.IV, 4)
                 .input(foil, Polybenzimidazole, 32)
-                .input(frameGt, Titanium , 16)
-                .input(MetaBlocks.METAL_CASING.getItemVariant(BlockMetalCasing.MetalCasingType.TITANIUM_STABLE).getItem(),2)
+                .input(frameGt, Titanium, 16)
+                .inputs(MetaBlocks.METAL_CASING.getItemVariant(BlockMetalCasing.MetalCasingType.STAINLESS_CLEAN))
                 .output(GTBMetaTileEntities.ENGRAVING_UNIT)
                 .duration(120)
                 .EUt(800)
                 .buildAndRegister();
 
-        
-
-
-
-
-
-
-
-
-        ModHandler.addShapedRecipe("water_tank", GTBMetaTileEntities.WATER_TANK.getStackForm(1),
+        ModHandler.addShapedRecipe("water_tank_machine", GTBMetaTileEntities.WATER_TANK.getStackForm(1),
                 "WPW", "FSF", "WCW",
                 'W', OreDictUnifier.get(plate, Wood),
                 'C', OreDictUnifier.get(circuit, MarkerMaterials.Tier.LV),
                 'P', ELECTRIC_PUMP_LV,
                 'F', OreDictUnifier.get(frameGt, Steel));
 
-        ModHandler.addShapedRecipe("clarifier", GTBMetaTileEntities.CLARIFIER.getStackForm(1),
+        ModHandler.addShapedRecipe("clarifier_machine", GTBMetaTileEntities.CLARIFIER.getStackForm(1),
                 "PCP", "UMU", "PCP",
                 'M', MetaBlocks.MACHINE_CASING.getItemVariant(BlockMachineCasing.MachineCasingType.EV),
                 'C', OreDictUnifier.get(circuit, MarkerMaterials.Tier.LV),
                 'P', PISTON,
                 'U', PUMP);
 
-        ModHandler.addShapedRecipe("digester", GTBMetaTileEntities.DIGESTER.getStackForm(1),
+        ModHandler.addShapedRecipe("digester_machine", GTBMetaTileEntities.DIGESTER.getStackForm(1),
                 "TTT", "PMP", "TTT",
                 'M', MetaBlocks.MACHINE_CASING.getItemVariant(BlockMachineCasing.MachineCasingType.IV),
                 'P', PISTON,
                 'T', OreDictUnifier.get(plate, Titanium));
 
-        ModHandler.addShapedRecipe("ffu", GTBMetaTileEntities.FROTH_FLOTATION_UNIT.getStackForm(1),
+        ModHandler.addShapedRecipe("ffu_machine", GTBMetaTileEntities.FROTH_FLOTATION_UNIT.getStackForm(1),
                 "CPC", "PMP", "CPC",
                 'M', MetaBlocks.MACHINE_CASING.getItemVariant(BlockMachineCasing.MachineCasingType.EV),
                 'C', OreDictUnifier.get(circuit, MarkerMaterials.Tier.LV),
                 'P', PUMP);
-
-
-
-
-
-
-
-
-
-
-
-
 
         registerMachineRecipe(GTBMetaTileEntities.ROTARY_EVAPORATOR,
                 "PRP", "CMC", "PRP",
@@ -148,8 +123,8 @@ public class MachineRecipes {
 
         registerMachineRecipe(GTBMetaTileEntities.CVD_UNIT,
                 "CGG",
-                        "KMP",
-                        "CHP",
+                "KMP",
+                "CHP",
                 'C', CIRCUIT,
                 'H', HULL,
                 'G', GLASS,
@@ -195,9 +170,5 @@ public class MachineRecipes {
                 'C', CIRCUIT,
                 'K', CABLE,
                 'M', HULL);
-
-
-
-
     }
 }
