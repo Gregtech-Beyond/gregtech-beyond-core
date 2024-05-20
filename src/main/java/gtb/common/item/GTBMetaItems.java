5a4bad54317582f62fe17badd4453e640877c814
BLocky104 <farmermick13@web.de>
2024-05-20 17:57:21 +0200

    MetaItems registry

--- src/main/java/gtb/common/item/GTBMetaItems.java
+++ src/main/java/gtb/common/item/GTBMetaItems.java
@@ -177,6 +177,12 @@ public class GTBMetaItems {
     public static MetaItem<?>.MetaValueItem HOT_MODERATELY_FORGED_DAMASCUS_STEEL_ASSEMBLY;
     public static MetaItem<?>.MetaValueItem HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY;
     public static MetaItem<?>.MetaValueItem HOT_HIGHLY_FORGED_DAMASCUS_STEEL_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem COOLANT_CELL;
+    public static MetaItem<?>.MetaValueItem NAK_COOLANT_CELL;
+    public static MetaItem<?>.MetaValueItem BORON_RETAINING_YARN;
+    public static MetaItem<?>.MetaValueItem BORON_SATURATED_YARN;
+    public static MetaItem<?>.MetaValueItem LITHIUM_SIEVE;
+    public static MetaItem<?>.MetaValueItem LITHIUM_SATURATED_LITHIUM_SIEVE;
 
     public static void init() {
         GTBMetaItemRegistry metaItem1 = new GTBMetaItemRegistry((short) 0);
