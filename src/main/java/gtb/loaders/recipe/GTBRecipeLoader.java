38e75c1c2cb0321afa6d41df0c9bcc59bace413f
BLocky104 <farmermick13@web.de>
2024-05-22 22:14:28 +0200

    update Laser Engraver Recipemap

--- src/main/java/gtb/loaders/recipe/GTBRecipeLoader.java
+++ src/main/java/gtb/loaders/recipe/GTBRecipeLoader.java
@@ -15,6 +15,8 @@ public final class GTBRecipeLoader {
         RecipeMaps.MIXER_RECIPES.setMaxFluidInputs(4);
         RecipeMaps.EXTRACTOR_RECIPES.setMaxInputs(2);
         RecipeMaps.CHEMICAL_RECIPES.setMaxOutputs(3);
+        RecipeMaps.LASER_ENGRAVER_RECIPES.setMaxFluidOutputs(1);
+        RecipeMaps.LASER_ENGRAVER_RECIPES.setMaxFluidInputs(1);
 
         AlcoholLine.init();
         Catalysts.init();
