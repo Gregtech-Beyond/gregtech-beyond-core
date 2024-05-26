eac4d16236e5664e491fc8f950ab85c19f8ce944
BLocky104 <farmermick13@web.de>
2024-05-26 23:42:08 +0200

    add Monazite Line

--- src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterialPropertyAddition.java
@@ -17,13 +17,37 @@ public class GTBMaterialPropertyAddition {
         Nihonium.setProperty(PropertyKey.DUST, new DustProperty());
         Thallium.setProperty(PropertyKey.DUST, new DustProperty());
         Iodine.setProperty(PropertyKey.DUST, new DustProperty());
+        Titanium.addFlags(MaterialFlags.GENERATE_FOIL);
         NaquadahAlloy.addFlags(MaterialFlags.GENERATE_FINE_WIRE);
         Graphene.addFlags(MaterialFlags.GENERATE_FOIL);
         Mica.addFlags(MaterialFlags.GENERATE_FOIL);
         BorosilicateGlass.addFlags(MaterialFlags.GENERATE_PLATE);
         Cupronickel.addFlags(MaterialFlags.GENERATE_LONG_ROD);
         AmmoniumChloride.setProperty(PropertyKey.FLUID, new FluidProperty());
-        // Graphene.setProperty(PropertyKey.INGOT, new IngotProperty());
         Mica.setProperty(PropertyKey.INGOT, new IngotProperty());
+
+        // Pre-Registered Materials ????????
+
+        // Zirconium
+        Zirconium.setProperty(PropertyKey.FLUID, new FluidProperty());
+        Zirconium.setProperty(PropertyKey.INGOT, new IngotProperty());
+        Zirconium.addFlags(MaterialFlags.GENERATE_PLATE);
+        Zirconium.addFlags(MaterialFlags.GENERATE_FRAME);
+        Zirconium.addFlags(MaterialFlags.GENERATE_ROD);
+
+        // Holmium
+
+        Holmium.setProperty(PropertyKey.INGOT, new IngotProperty());
+        Holmium.addFlags(MaterialFlags.GENERATE_PLATE);
+        Holmium.addFlags(MaterialFlags.GENERATE_FRAME);
+        Holmium.addFlags(MaterialFlags.GENERATE_ROD);
+
+        // Hafnium
+
+        Hafnium.setProperty(PropertyKey.FLUID, new FluidProperty());
+        Hafnium.setProperty(PropertyKey.INGOT, new IngotProperty());
+        Hafnium.addFlags(MaterialFlags.GENERATE_PLATE);
+        Hafnium.addFlags(MaterialFlags.GENERATE_FRAME);
+        Hafnium.addFlags(MaterialFlags.GENERATE_ROD);
     }
 }
--- src/main/java/gtb/api/unification/materials/GTBMaterials.java
+++ src/main/java/gtb/api/unification/materials/GTBMaterials.java
@@ -106,11 +106,53 @@ public class GTBMaterials {
     // Element Materials
     public static Material Rubidium;
     public static Material Holmium;
-    // FD Materials
 
+    // FD Materials
+    public static Material Tetradecanol;
+    public static Material MethylIsobutylCarbonyl;
+    public static Material AmidoEthylAlkylImidazole;
+    public static Material DiacetoneAlcohol;
+    public static Material Octanol;
+    public static Material PalmaticAcid;
+    public static Material ZieglerAlfolMixture;
+    public static Material Dodecanol;
+    public static Material MonaziteSolution;
+    public static Material MuddyMonaziteSolution;
+    public static Material Decanol;
+    public static Material Hexanol;
+    public static Material CaprylicAcid;
+    public static Material MonaziteOreByproductSlurry;
+    public static Material ImpureMonaziteOreByproductSlurry;
+    public static Material MuddyMonaziteOreByproducts;
+    public static Material DilutedCeriumFiltrate;
+    public static Material CeriumOxideFiltrate;
+    public static Material CooledRareEarthFiltrate;
+    public static Material NeutralizedRareEarthFiltrate;
+    public static Material RareEarthFiltrate;
+    public static Material DilutedMonaziteSulfide;
+    public static Material MonaziteSulfide;
+    public static Material ZincChloride;
+    public static Material ThoriumPhosphateCakeDust;
+    public static Material DriedMonaziteFiltrate;
+    public static Material EuropiumOxide;
+    public static Material ThoriumConcentrate;
+    public static Material AluminiumHydroxide;
+    public static Material GadoliniumOxide;
+    public static Material MonaziteRarerEarthFiltrate;
+    public static Material ChlorinatedMonaziteFiltrate;
+    public static Material HafniumOxide;
+    public static Material HafniumZirconiumSlag;
+    public static Material WetHafniumZirconiumBlend;
+    public static Material SaturatedChlorinatedMonaziteFiltrate;
+    public static Material RoastedHafniumZirconiumSlag;
+    public static Material ZirconiumOxide;
     public static Material CeriumOxide;
+    public static Material RutherfordiumOxide;
+    public static Material ThoriumPhosphateCake;
+    public static Material UraniumFiltrate;
     public static Material DilutedCeriumSolution;
     public static Material CeriumSolution;
+    public static Material Hexadecanol;
     public static Material CalcinatedBastnasite;
     public static Material CalcinatedSiliconDioxide;
     public static Material CalcinatedResidues;
@@ -582,6 +624,7 @@ public class GTBMaterials {
     public static Material Kevlar;
     public static Material KevlarConcentrate;
     public static Material KevlarBlend;
+    public static Material PurifiedKevlarConcentrate;
 
     // Fantasy Materials
     public static Material AwakenedDraconium;
