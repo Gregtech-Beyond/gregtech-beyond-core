7582ab748755500e4c0fb810e95b7a7ade67ac99
BLocky104 <farmermick13@web.de>
2024-05-27 00:24:37 +0200

    PVC

--- src/main/java/gtb/loaders/recipe/PolymerizationRework.java
+++ src/main/java/gtb/loaders/recipe/PolymerizationRework.java
@@ -42,17 +42,6 @@ public class PolymerizationRework {
         POLYMERIZATION_TANK.recipeBuilder().input(dust, ZieglerNattaCatalyst).fluidInputs(VinylAcetate.getFluid(144))
                 .fluidOutputs(PolyvinylAcetate.getFluid(432)).duration(500).EUt(800).buildAndRegister();
 
-        POLYMERIZATION_TANK.recipeBuilder().fluidInputs(Air.getFluid(1000)).fluidInputs(VinylChloride.getFluid(144))
-                .fluidOutputs(PolyvinylChloride.getFluid(144)).duration(500).EUt(800).buildAndRegister();
-        POLYMERIZATION_TANK.recipeBuilder().fluidInputs(Oxygen.getFluid(1000)).fluidInputs(VinylChloride.getFluid(144))
-                .fluidOutputs(PolyvinylChloride.getFluid(144)).duration(500).EUt(800).buildAndRegister();
-        POLYMERIZATION_TANK.recipeBuilder().input(dust, KaminskyCatalyst).fluidInputs(VinylChloride.getFluid(144))
-                .fluidOutputs(PolyvinylChloride.getFluid(576)).duration(500).EUt(800).buildAndRegister();
-        POLYMERIZATION_TANK.recipeBuilder().input(dust, PhilipsCatalyst).fluidInputs(VinylChloride.getFluid(144))
-                .fluidOutputs(PolyvinylChloride.getFluid(288)).duration(500).EUt(800).buildAndRegister();
-        POLYMERIZATION_TANK.recipeBuilder().input(dust, ZieglerNattaCatalyst).fluidInputs(VinylChloride.getFluid(144))
-                .fluidOutputs(PolyvinylChloride.getFluid(432)).duration(500).EUt(800).buildAndRegister();
-
         GTRecipeHandler.removeRecipesByInputs(CHEMICAL_RECIPES,
                 new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(1) },
                 new FluidStack[] { Air.getFluid(1000), Ethylene.getFluid(144) });
@@ -78,20 +67,6 @@ public class PolymerizationRework {
         GTRecipeHandler.removeRecipesByInputs(LARGE_CHEMICAL_RECIPES,
                 new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(2) }, new FluidStack[] {
                         Oxygen.getFluid(7500), VinylAcetate.getFluid(2160), TitaniumTetrachloride.getFluid(100) });
-
-        GTRecipeHandler.removeRecipesByInputs(CHEMICAL_RECIPES,
-                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(1) },
-                new FluidStack[] { Air.getFluid(1000), VinylChloride.getFluid(144) });
-        GTRecipeHandler.removeRecipesByInputs(CHEMICAL_RECIPES,
-                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(1) },
-                new FluidStack[] { Oxygen.getFluid(1000), VinylChloride.getFluid(144) });
-        GTRecipeHandler.removeRecipesByInputs(LARGE_CHEMICAL_RECIPES,
-                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(2) }, new FluidStack[] { Air.getFluid(7500),
-                        VinylChloride.getFluid(2160), TitaniumTetrachloride.getFluid(100) });
-        GTRecipeHandler.removeRecipesByInputs(LARGE_CHEMICAL_RECIPES,
-                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(2) }, new FluidStack[] {
-                        Oxygen.getFluid(7500), VinylChloride.getFluid(2160), TitaniumTetrachloride.getFluid(100) });
-
 
 
 
---
+++ src/main/java/gtb/loaders/recipe/polymerLines/PolyvinylChlorideLine.java
@@ -0,0 +1,46 @@
+package gtb.loaders.recipe.polymerLines;
+
+import gregtech.api.recipes.GTRecipeHandler;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
+
+public class PolyvinylChlorideLine {
+
+    public static void init(){
+
+        GTRecipeHandler.removeRecipesByInputs(CHEMICAL_RECIPES,
+                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(1) },
+                new FluidStack[] { Air.getFluid(1000), VinylChloride.getFluid(144) });
+        GTRecipeHandler.removeRecipesByInputs(CHEMICAL_RECIPES,
+                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(1) },
+                new FluidStack[] { Oxygen.getFluid(1000), VinylChloride.getFluid(144) });
+        GTRecipeHandler.removeRecipesByInputs(LARGE_CHEMICAL_RECIPES,
+                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(2) }, new FluidStack[] { Air.getFluid(7500),
+                        VinylChloride.getFluid(2160), TitaniumTetrachloride.getFluid(100) });
+        GTRecipeHandler.removeRecipesByInputs(LARGE_CHEMICAL_RECIPES,
+                new ItemStack[] { IntCircuitIngredient.getIntegratedCircuit(2) }, new FluidStack[] {
+                        Oxygen.getFluid(7500), VinylChloride.getFluid(2160), TitaniumTetrachloride.getFluid(100) });
+
+
+        POLYMERIZATION_TANK.recipeBuilder().fluidInputs(Air.getFluid(1000)).fluidInputs(VinylChloride.getFluid(144))
+                .fluidOutputs(PolyvinylChloride.getFluid(144)).duration(500).EUt(800).buildAndRegister();
+        POLYMERIZATION_TANK.recipeBuilder().fluidInputs(Oxygen.getFluid(1000)).fluidInputs(VinylChloride.getFluid(144))
+                .fluidOutputs(PolyvinylChloride.getFluid(144)).duration(500).EUt(800).buildAndRegister();
+        POLYMERIZATION_TANK.recipeBuilder().input(dust, KaminskyCatalyst).fluidInputs(VinylChloride.getFluid(144))
+                .fluidOutputs(PolyvinylChloride.getFluid(576)).duration(500).EUt(800).buildAndRegister();
+        POLYMERIZATION_TANK.recipeBuilder().input(dust, PhilipsCatalyst).fluidInputs(VinylChloride.getFluid(144))
+                .fluidOutputs(PolyvinylChloride.getFluid(288)).duration(500).EUt(800).buildAndRegister();
+        POLYMERIZATION_TANK.recipeBuilder().input(dust, ZieglerNattaCatalyst).fluidInputs(VinylChloride.getFluid(144))
+                .fluidOutputs(PolyvinylChloride.getFluid(432)).duration(500).EUt(800).buildAndRegister();
+
+
+
+    }
+}
