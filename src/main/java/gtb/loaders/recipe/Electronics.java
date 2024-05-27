48b0a57402f676ea9d6b3e1c3d987b3e6816fc78
BLocky104 <farmermick13@web.de>
2024-05-27 03:49:39 +0200

    add recipe for Rotarry Spinning Whisker

---
+++ src/main/java/gtb/loaders/recipe/Electronics.java
@@ -0,0 +1,26 @@
+package gtb.loaders.recipe;
+
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gregtech.common.items.MetaItems.*;
+import static gtb.common.item.GTBMetaItems.*;
+
+
+public class Electronics {
+
+    public static void init()  {
+
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .input(plate, Polybenzimidazole)
+                .input(ELECTRIC_MOTOR_HV)
+                .input(dye, DyeOrange)
+                .input(stick, Steel, 4)
+                .output(ROTARRY_SPINNING_WHISKER)
+                .duration(2000)
+                .EUt(700)
+                .buildAndRegister();
+
+    }
+}
