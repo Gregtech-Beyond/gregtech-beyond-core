138f2ff306b23387cc057ca18bce951bdec5c60f
BLocky104 <farmermick13@web.de>
2024-05-29 01:24:07 +0200

    add recipes for Sintered Silver

--- src/main/java/gtb/loaders/recipe/Electronics.java
+++ src/main/java/gtb/loaders/recipe/Electronics.java
@@ -4,6 +4,8 @@ import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.*;
 import static gregtech.common.items.MetaItems.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
 import static gtb.common.item.GTBMetaItems.*;
 
 public class Electronics {
@@ -17,6 +19,15 @@ public class Electronics {
                 .output(ROTARRY_SPINNING_WHISKER)
                 .duration(2000)
                 .EUt(700)
+                .buildAndRegister();
+
+        SINTERING_OVEN_RECIPES.recipeBuilder()
+                .fluidInputs(Methane.getFluid(1000))
+                .notConsumable(SHAPE_MOLD_INGOT)
+                .input(dust, Silver)
+                .output(ingot, SinteredSilver)
+                .duration(200)
+                .EUt(800)
                 .buildAndRegister();
     }
 }
