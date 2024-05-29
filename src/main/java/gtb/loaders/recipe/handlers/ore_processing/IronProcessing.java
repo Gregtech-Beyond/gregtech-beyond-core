80cfd360f5e55d7dd867d1cf5238332c4a6dafc7
BLocky104 <farmermick13@web.de>
2024-05-29 03:23:11 +0200

    add Iron Processing
    -Yellow Limonite
    -Banded Iron
    -Brown Limonite
    -Magnetite
    -Basaltic Mineral Sand

---
+++ src/main/java/gtb/loaders/recipe/handlers/ore_processing/IronProcessing.java
@@ -0,0 +1,116 @@
+package gtb.loaders.recipe.handlers.ore_processing;
+
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+import gregtech.api.recipes.ModHandler;
+import gregtech.api.unification.OreDictUnifier;
+
+public class IronProcessing {
+
+    public static void init() {
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustImpure, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dust, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustPure, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushed, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedCentrifuged, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, Magnetite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, Magnetite));
+
+        BLAST_RECIPES.recipeBuilder()
+                .fluidInputs(Hydrogen.getFluid(1000))
+                .fluidOutputs(Steam.getFluid(1000))
+                .input(dust, Magnetite)
+                .output(ingot, Iron)
+                .duration(200)
+                .EUt(12)
+                .blastFurnaceTemp(1000)
+                .buildAndRegister();
+
+        PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
+                .input(dust, Magnetite)
+                .output(ingot, BandedIron)
+                .duration(200)
+                .buildAndRegister();
+
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustImpure, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dust, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustPure, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushed, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedCentrifuged, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, BandedIron));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, BandedIron));
+
+        BLAST_RECIPES.recipeBuilder()
+                .fluidInputs(Hydrogen.getFluid(1000))
+                .fluidOutputs(Steam.getFluid(1000))
+                .input(ingot, BandedIron)
+                .output(ingot, Iron)
+                .duration(200)
+                .EUt(12)
+                .blastFurnaceTemp(1000)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .fluidInputs(Hydrogen.getFluid(1000))
+                .fluidOutputs(Steam.getFluid(1000))
+                .input(dust, BandedIron)
+                .output(ingot, Iron)
+                .duration(200)
+                .EUt(12)
+                .blastFurnaceTemp(1000)
+                .buildAndRegister();
+
+        ROASTER_RECIPES.recipeBuilder()
+                .fluidInputs(Oxygen.getFluid(100))
+                .input(dust, BandedIron)
+                .output(ingot, Iron)
+                .EUt(8)
+                .duration(80)
+                .buildAndRegister();
+
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustImpure, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dust, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustPure, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushed, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedCentrifuged, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, YellowLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, YellowLimonite));
+
+        ROASTER_RECIPES.recipeBuilder()
+                .input(dust, YellowLimonite, 4)
+                .fluidInputs(Hydrogen.getFluid(3000))
+                .output(dust, Iron)
+                .fluidOutputs(Steam.getFluid(2000))
+                .duration(200)
+                .EUt(9)
+                .buildAndRegister();
+
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustImpure, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dust, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustPure, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushed, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedCentrifuged, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, BrownLimonite));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, BrownLimonite));
+
+        ROASTER_RECIPES.recipeBuilder()
+                .input(dust, BrownLimonite, 4)
+                .fluidInputs(Hydrogen.getFluid(3000))
+                .output(dust, Iron)
+                .fluidOutputs(Steam.getFluid(2000))
+                .duration(200)
+                .EUt(9)
+                .buildAndRegister();
+
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustImpure, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dust, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(dustPure, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushed, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedCentrifuged, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, BasalticMineralSand));
+        ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, BasalticMineralSand));
+    }
+}
