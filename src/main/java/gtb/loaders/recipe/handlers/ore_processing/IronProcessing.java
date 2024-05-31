7ee3be599151ab71ced61b78ddafe1f8c4907144
BLocky104 <farmermick13@web.de>
2024-05-31 14:51:53 +0200

    Iron Processing Overhaul

--- src/main/java/gtb/loaders/recipe/handlers/ore_processing/IronProcessing.java
+++ src/main/java/gtb/loaders/recipe/handlers/ore_processing/IronProcessing.java
@@ -3,14 +3,11 @@ package gtb.loaders.recipe.handlers.ore_processing;
 import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.*;
-import static gregtech.common.items.MetaItems.NANO_CENTRAL_PROCESSING_UNIT_WAFER;
 import static gtb.api.recipes.GTBRecipeMaps.*;
-
 import gregtech.api.recipes.GTRecipeHandler;
 import gregtech.api.recipes.ModHandler;
 import gregtech.api.unification.OreDictUnifier;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
 
 public class IronProcessing {
 
@@ -139,8 +136,12 @@ public class IronProcessing {
         ModHandler.removeFurnaceSmelting(OreDictUnifier.get(crushedPurified, BasalticMineralSand));
         ModHandler.removeFurnaceSmelting(OreDictUnifier.get(ore, BasalticMineralSand));
 
-        GTRecipeHandler.removeRecipesByInputs(ELECTROLYTIC_CELL_RECIPES,
+        GTRecipeHandler.removeRecipesByInputs(ELECTROLYZER_RECIPES,
                 new ItemStack[] {
                         OreDictUnifier.get(dust, BandedIron, 5) });
+
+        GTRecipeHandler.removeRecipesByInputs(ELECTROLYZER_RECIPES,
+                new ItemStack[] {
+                        OreDictUnifier.get(dust, Magnetite, 7) });
     }
 }
