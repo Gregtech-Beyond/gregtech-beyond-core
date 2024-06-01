d263d94433d2629319804136b590c500145c742d
BLocky104 <farmermick13@web.de>
2024-06-01 02:57:47 +0200

    add Zinc Processing

---
+++ src/main/java/gtb/loaders/recipe/handlers/ore_processing/ZincProcessing.java
@@ -0,0 +1,51 @@
+package gtb.loaders.recipe.handlers.ore_processing;
+
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import gregtech.api.recipes.GTRecipeHandler;
+import gregtech.api.unification.OreDictUnifier;
+import gregtech.common.items.MetaItems;
+
+
+public class ZincProcessing {
+
+    public static void init(){
+
+
+
+        GTRecipeHandler.removeRecipesByInputs(ELECTROLYZER_RECIPES,
+                new ItemStack[] {
+                        OreDictUnifier.get(dust, Sphalerite, 2) });
+
+        GTRecipeHandler.removeRecipesByInputs(ELECTROLYZER_RECIPES,
+                new ItemStack[] {
+                        OreDictUnifier.get(dust, Zincite, 2) });
+
+        ROASTER_RECIPES.recipeBuilder()
+                .input(dust, Zincite, 2)
+                .output(dust, Zinc)
+                .fluidOutputs(Steam.getFluid(1000))
+                .circuitMeta(1)
+                .duration(20)
+                .EUt(7)
+                .buildAndRegister();
+
+        ROASTER_RECIPES.recipeBuilder()
+                .input(dust, Zincite, 2)
+                .output(dust, Zinc)
+                .circuitMeta(0)
+                .duration(20)
+                .EUt(7)
+                .buildAndRegister();
+
+
+
+    }
+}
