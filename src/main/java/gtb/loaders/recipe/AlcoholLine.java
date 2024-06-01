c8776646013f006ed69cd4c9126d7e6e3ccb7a24
BLocky104 <farmermick13@web.de>
2024-06-01 07:01:13 +0200

    change some Values for Alcohol Production

--- src/main/java/gtb/loaders/recipe/AlcoholLine.java
+++ src/main/java/gtb/loaders/recipe/AlcoholLine.java
@@ -16,7 +16,7 @@ public class AlcoholLine {
                 .fluidOutputs(EthanolWaterAzeotrope.getFluid(720))
                 .fluidOutputs(FuselOil.getFluid(80))
                 .duration(120)
-                .EUt(800)
+                .EUt(20)
                 .buildAndRegister();
 
         CHEMICAL_RECIPES.recipeBuilder()
@@ -26,7 +26,7 @@ public class AlcoholLine {
                 .fluidOutputs(Ethylene.getFluid(1200))
                 .duration(200)
                 .circuitMeta(1)
-                .EUt(800)
+                .EUt(20)
                 .buildAndRegister();
 
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
@@ -36,7 +36,7 @@ public class AlcoholLine {
                 .fluidOutputs(Ethylene.getFluid(1200))
                 .duration(200)
                 .circuitMeta(1)
-                .EUt(800)
+                .EUt(40)
                 .buildAndRegister();
 
         DISTILLATION_RECIPES.recipeBuilder()
@@ -77,7 +77,7 @@ public class AlcoholLine {
                 .fluidOutputs(AceticAcid.getFluid(400))
                 .output(dust, Wood, 1)
                 .duration(200)
-                .EUt(800)
+                .EUt(20)
                 .buildAndRegister();
     }
 }
