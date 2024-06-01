9b64bb0373156705951f15c0d5df37b55e1179b1
BLocky104 <farmermick13@web.de>
2024-06-01 21:43:37 +0200

    optimized OrePrefix Code

--- src/main/java/gtb/api/unification/ore/GTBOrePrefix.java
+++ src/main/java/gtb/api/unification/ore/GTBOrePrefix.java
@@ -1,19 +1,13 @@
 package gtb.api.unification.ore;
 
 import static gregtech.api.GTValues.*;
-import static gregtech.api.unification.ore.OrePrefix.Conditions.*;
-import static gregtech.api.unification.ore.OrePrefix.Flags.*;
 import static gtb.api.unification.materials.info.GTBMaterialFlags.*;
-
 import gregtech.api.unification.ore.OrePrefix;
-
 import gtb.api.unification.materials.info.GTBMaterialIconType;
 
 public class GTBOrePrefix {
 
-    public static final OrePrefix floated = new OrePrefix("floated", M * 1, null, GTBMaterialIconType.floated,
-            ENABLE_UNIFICATION, hasDustProperty);
-
-    public static final OrePrefix sifted = new OrePrefix("sifted", M * 1, null, GTBMaterialIconType.sifted,
-            ENABLE_UNIFICATION, hasDustProperty);
+    public static final OrePrefix floated = new OrePrefix("floated", M * 1, null, GTBMaterialIconType.floated, OrePrefix.Flags.ENABLE_UNIFICATION    , mat -> mat.hasFlag(GENERATE_FLOATED));
+    public static final OrePrefix sifted = new OrePrefix("sifted", M * 1, null,  GTBMaterialIconType.sifted, OrePrefix.Flags.ENABLE_UNIFICATION    , mat -> mat.hasFlag(GENERATE_FLOATED));
+    
 }
