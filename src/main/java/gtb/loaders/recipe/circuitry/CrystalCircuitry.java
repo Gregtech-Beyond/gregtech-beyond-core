1aff7ce5c5bc0f7b4d38e42ec85b828eac5584c7
BLocky104 <farmermick13@web.de>
2024-06-09 04:55:12 +0200

    Crystal Circuitry Rework

---
+++ src/main/java/gtb/loaders/recipe/circuitry/CrystalCircuitry.java
@@ -0,0 +1,46 @@
+package gtb.loaders.recipe.circuitry;
+
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gregtech.common.items.MetaItems.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
+
+import gregtech.api.recipes.GTRecipeHandler;
+import gregtech.api.unification.OreDictUnifier;
+import gregtech.api.unification.material.MarkerMaterials;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import static gtb.common.item.GTBMetaItems.*;
+
+public class CrystalCircuitry {
+    public static void init(){
+
+        GTRecipeHandler.removeRecipesByInputs(LASER_ENGRAVER_RECIPES,
+                new ItemStack[] {
+                        OreDictUnifier.get(craftingLens, MarkerMaterials.Color.Lime),
+                        ENGRAVED_CRYSTAL_CHIP.getStackForm()});
+
+        LASER_ENGRAVER_RECIPES.recipeBuilder()
+                .input(ENGRAVED_CRYSTAL_CHIP)
+                .notConsumable(BIOLOGICALLY_APPROVED_LENS)
+                .output(CRYSTAL_CENTRAL_PROCESSING_UNIT)
+                .duration(200)
+                .EUt(80)
+                .buildAndRegister();
+
+        CHEMICAL_BATH_RECIPES.recipeBuilder()
+                .fluidInputs(Xerizin.getFluid(1000))
+                .input(craftingLens, MarkerMaterials.Color.Black)
+                .duration(200)
+                .output(BIOLOGICALLY_APPROVED_LENS)
+                .EUt(120)
+                .buildAndRegister();
+
+
+
+
+    }
+}
