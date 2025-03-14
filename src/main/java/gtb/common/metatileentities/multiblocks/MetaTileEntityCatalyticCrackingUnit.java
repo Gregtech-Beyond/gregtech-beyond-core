76925a2fcc63564d94ba043dd4ba3a22815c9379
BLocky104 <farmermick13@web.de>
2025-03-14 20:58:25 +0100

    add Catalytic Cracking Unit

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityCatalyticCrackingUnit.java
@@ -0,0 +1,105 @@
+package gtb.common.metatileentities.multiblocks;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockBoilerCasing.BoilerCasingType;
+import gregtech.common.blocks.BlockFireboxCasing;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
+import gtb.api.NoEnergyLogic;
+import gtb.api.recipes.GTBRecipeMaps;
+
+public class MetaTileEntityCatalyticCrackingUnit extends RecipeMapMultiblockController {
+
+    public MetaTileEntityCatalyticCrackingUnit(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.CATALYTIC_CRACKING_RECIPES);
+        this.recipeMapWorkable = new NoEnergyLogic(this);
+        initializeAbilities();
+    }
+
+    public IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STAINLESS_CLEAN);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC", "CCCCC",
+                        "CCCCC", "CCCCC", "CCCCC")
+                .aisle("CCCCC", " AAA ", " AAA ", " AAA ", " P P ", " P P ", " P P ", " P P ", "CP PC", " AAA ",
+                        " AAA ", " AAA ", "CCSCC")
+                .aisle("CCCCC", " AAA ", " A A ", " AAA ", "     ", "     ", "     ", "     ", "C   C", " AAA ",
+                        " F F ", " AAA ", "CCCCC")
+                .aisle("CCCCC", " TTT ", " TTT ", " TTT ", "  R  ", "  R  ", "  R  ", "  R  ", "C R C", " AAA ",
+                        " AAA ", " AAA ", "CCCCC")
+                .aisle("CCCCC", "C   C", "C   C", "C   C", "C   C", "C   C", "C   C", "C   C", "CCCCC", "CCCCC",
+                        "CCCCC", "CCCCC", "CCCCC")
+                .where('S', selfPredicate())
+                .where('G', states(Blocks.GLASS.getDefaultState()))
+                .where('~', any())
+                .where('C', states(getCasingState())
+                        .or(abilities(MultiblockAbility.EXPORT_FLUIDS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.IMPORT_ITEMS).setExactLimit(1)))
+                .where('F',
+                        states(MetaBlocks.BOILER_FIREBOX_CASING
+                                .getState(BlockFireboxCasing.FireboxCasingType.STEEL_FIREBOX)))
+                .where('P', states(MetaBlocks.BOILER_CASING.getState((BoilerCasingType.STEEL_PIPE))))
+                .where('R', states(MetaBlocks.BOILER_CASING.getState((BoilerCasingType.BRONZE_PIPE))))
+                .where('A', states(MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID)))
+                .where('T',
+                        states(MetaBlocks.BOILER_FIREBOX_CASING
+                                .getState(BlockFireboxCasing.FireboxCasingType.BRONZE_FIREBOX)))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(false, false, true, false, false, true, false);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.SOLID_STEEL_CASING;
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        getFrontOverlay().renderOrientedState(renderState, translation, pipeline, getFrontFacing(),
+                recipeMapWorkable.isActive(), recipeMapWorkable.isWorkingEnabled());
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.ASSEMBLER_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityCoker(metaTileEntityId);
+    }
+}
