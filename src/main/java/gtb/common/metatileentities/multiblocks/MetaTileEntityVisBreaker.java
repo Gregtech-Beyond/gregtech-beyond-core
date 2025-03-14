6af69e5aa4612e9386c2439e2952b9f5766570e0
BLocky104 <farmermick13@web.de>
2025-03-14 20:58:44 +0100

    add Vis Breaker

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityVisBreaker.java
@@ -0,0 +1,98 @@
+package gtb.common.metatileentities.multiblocks;
+
+import static gregtech.api.unification.material.Materials.Steel;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
+import gtb.api.recipes.GTBRecipeMaps;
+
+public class MetaTileEntityVisBreaker extends RecipeMapMultiblockController {
+
+    public MetaTileEntityVisBreaker(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.VIS_BREAKER_RECIPES);
+        initializeAbilities();
+    }
+
+    public IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("~~~~~~~", "~~CCC~~", "~CCCCC~", "~CCCCC~", "~CCCCC~", "~~CSC~~", "~~~~~~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~FFF~~", "~FFFFF~", "FFCCCFF", "FFCCCFF", "FFCCCFF", "~FFFFF~", "~~FFF~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~FFF~~", "~FFFFF~", "FFCCCFF", "FFCCCFF", "FFCCCFF", "~FFFFF~", "~~FFF~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~FFF~~", "~FFFFF~", "FFCCCFF", "FFCCCFF", "FFCCCFF", "~FFFFF~", "~~FFF~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~~~~~~", "~~CCC~~", "~C~~~C~", "~C~~~C~", "~C~~~C~", "~~CCC~~", "~~~~~~~")
+                .aisle("~~~~~~~", "~~~~~~~", "~~CCC~~", "~~CCC~~", "~~CCC~~", "~~~~~~~", "~~~~~~~")
+                .where('S', selfPredicate())
+                .where('~', any())
+                .where('C', states(getCasingState())
+                        .or(abilities(MultiblockAbility.EXPORT_FLUIDS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.IMPORT_ITEMS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.INPUT_ENERGY).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.IMPORT_FLUIDS).setExactLimit(1))
+                        .or(abilities(MultiblockAbility.EXPORT_ITEMS).setExactLimit(1)))
+                .where('F', frames(Steel))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(false, false, true, false, false, true, false);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.STEEL_FIREBOX;
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        getFrontOverlay().renderOrientedState(renderState, translation, pipeline, getFrontFacing(),
+                recipeMapWorkable.isActive(), recipeMapWorkable.isWorkingEnabled());
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.COMPRESSOR_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityVisBreaker(metaTileEntityId);
+    }
+}
