d616702d47c4148bc2ab81109f3c88ecff49d2b4
BLocky104 <farmermick13@web.de>
2025-03-27 16:30:07 +0100

    Update GTBFantasyMaterials.java

--- src/main/java/gtb/api/unification/materials/GTBFantasyMaterials.java
+++ src/main/java/gtb/api/unification/materials/GTBFantasyMaterials.java
@@ -1,72 +1,45 @@
 package gtb.api.unification.materials;
 
-import static gregtech.api.util.GTUtility.gregtechId;
+import gregtech.api.unification.material.Material;
+import gtb.api.utils.GTBUtil;
 import static gtb.api.unification.materials.GTBMaterials.*;
 
-import gregtech.api.unification.material.Material;
 
 public class GTBFantasyMaterials {
 
-    // Range 25001-25200
-    private static int startId = 2601;
-    private static final int END_ID = startId + 50;
+    public static void init() {
 
-    public static void register() {
-        AwakenedDraconium = new Material.Builder(getMaterialsId(), gregtechId("awakened_draconium"))
+
+        AwakenedDraconium = new Material.Builder(14002, GTBUtil.gtb("awakened_draconium"))
                 .ingot()
                 .build().setFormula("AwDr", true);
 
-        Adamantium = new Material.Builder(getMaterialsId(), gregtechId("adamantium"))
+        Adamantium = new Material.Builder(14003, GTBUtil.gtb("adamantium"))
                 .ingot()
                 .build().setFormula("Ad", true);
 
-        CosmicNeutronium = new Material.Builder(getMaterialsId(), gregtechId("cosmic_neutronium"))
+        CosmicNeutronium = new Material.Builder(14004, GTBUtil.gtb("cosmic_neutronium"))
                 .ingot()
                 .build().setFormula("SpNt", true);
 
-        Infinity = new Material.Builder(getMaterialsId(), gregtechId("infinity"))
+        Infinity = new Material.Builder(14005, GTBUtil.gtb("infinity"))
                 .ingot()
                 .build().setFormula("If", true);
 
-        Eternal = new Material.Builder(getMaterialsId(), gregtechId("eternal"))
+        Eternal = new Material.Builder(14006, GTBUtil.gtb("eternal"))
                 .ingot()
                 .build().setFormula("Et*", true);
 
-        TemporallyConstrainedAntiMatter = new Material.Builder(getMaterialsId(),
-                gregtechId("temporally_constrained_anti_matter"))
-                        .ingot()
-                        .build().setFormula("Am**", true);
+        TemporallyConstrainedAntiMatter = new Material.Builder(14007,
+                GTBUtil.gtb("temporally_constrained_anti_matter"))
+                .ingot()
+                .build().setFormula("Am**", true);
 
-        CataclysmInducedMixedMatter = new Material.Builder(getMaterialsId(),
-                gregtechId("cataclysm_induced_mixed_matter"))
-                        .ingot()
-                        .build().setFormula("Mm***", true);
+        CataclysmInducedMixedMatter = new Material.Builder(14008,
+                GTBUtil.gtb("cataclysm_induced_mixed_matter"))
+                .ingot()
+                .build().setFormula("Mm***", true);
 
-        NaquadahWaste = new Material.Builder(getMaterialsId(),
-                gregtechId("naquadah_waste"))
-                        .fluid()
-                        .color(0x2d690b).build();
-
-        RareNaquadahWaste = new Material.Builder(getMaterialsId(),
-                gregtechId("rare_naquadah_waste"))
-                        .fluid()
-                        .color(0x0c2100).build();
-
-        NaqHydrochloricAcid = new Material.Builder(getMaterialsId(),
-                gregtechId("naq_hydrochloric_acid"))
-                        .fluid()
-                        .color(0x1d4f00).build();
-
-        RareNaqFluoroantimonicAcid = new Material.Builder(getMaterialsId(),
-                gregtechId("rare_naq_fluoroantimonic_acid"))
-                        .fluid()
-                        .color(0xa1c9a5).build();
     }
-
-    private static int getMaterialsId() {
-        if (startId < END_ID) {
-            return startId++;
-        }
-        throw new ArrayIndexOutOfBoundsException();
-    }
+    )
 }
