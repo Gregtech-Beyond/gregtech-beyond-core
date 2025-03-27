4ecea1bf773d9b81f97d764968c6eb481b6c423e
BLocky104 <farmermick13@web.de>
2025-03-28 00:27:25 +0100

    readd Pikyonium

--- src/main/java/gtb/api/unification/materials/GTBFantasyMaterials.java
+++ src/main/java/gtb/api/unification/materials/GTBFantasyMaterials.java
@@ -1,45 +1,56 @@
 package gtb.api.unification.materials;
 
-import gregtech.api.unification.material.Material;
-import gtb.api.utils.GTBUtil;
+import static gregtech.api.util.GTUtility.gregtechId;
 import static gtb.api.unification.materials.GTBMaterials.*;
 
+import gregtech.api.unification.material.Material;
 
 public class GTBFantasyMaterials {
 
+    // Range 25001-25200
+    private static int startId = 2601;
+    private static final int END_ID = startId + 50;
+
     public static void init() {
-
-
-        AwakenedDraconium = new Material.Builder(14002, GTBUtil.gtb("awakened_draconium"))
+        AwakenedDraconium = new Material.Builder(getMaterialsId(), gregtechId("awakened_draconium"))
                 .ingot()
                 .build().setFormula("AwDr", true);
 
-        Adamantium = new Material.Builder(14003, GTBUtil.gtb("adamantium"))
+        Adamantium = new Material.Builder(getMaterialsId(), gregtechId("adamantium"))
                 .ingot()
                 .build().setFormula("Ad", true);
 
-        CosmicNeutronium = new Material.Builder(14004, GTBUtil.gtb("cosmic_neutronium"))
+        CosmicNeutronium = new Material.Builder(getMaterialsId(), gregtechId("cosmic_neutronium"))
                 .ingot()
                 .build().setFormula("SpNt", true);
 
-        Infinity = new Material.Builder(14005, GTBUtil.gtb("infinity"))
+        Infinity = new Material.Builder(getMaterialsId(), gregtechId("infinity"))
                 .ingot()
                 .build().setFormula("If", true);
 
-        Eternal = new Material.Builder(14006, GTBUtil.gtb("eternal"))
+        Eternal = new Material.Builder(getMaterialsId(), gregtechId("eternal"))
                 .ingot()
                 .build().setFormula("Et*", true);
 
-        TemporallyConstrainedAntiMatter = new Material.Builder(14007,
-                GTBUtil.gtb("temporally_constrained_anti_matter"))
-                .ingot()
-                .build().setFormula("Am**", true);
+        TemporallyConstrainedAntiMatter = new Material.Builder(getMaterialsId(),
+                gregtechId("temporally_constrained_anti_matter"))
+                        .ingot()
+                        .build().setFormula("Am**", true);
 
-        CataclysmInducedMixedMatter = new Material.Builder(14008,
-                GTBUtil.gtb("cataclysm_induced_mixed_matter"))
-                .ingot()
-                .build().setFormula("Mm***", true);
+        CataclysmInducedMixedMatter = new Material.Builder(getMaterialsId(),
+                gregtechId("cataclysm_induced_mixed_matter"))
+                        .ingot()
+                        .build().setFormula("Mm***", true);
 
+        Pikyonium = new Material.Builder(getMaterialsId(), gregtechId("pikyonium"))
+                .ingot()
+                .build().setFormula("(Ni2NbAl2(Ni4Cr))8((Fe4(FeAlCr)(Fe2Ni05)10SSiC)5?4Ce3Sb2Pt2Yb(FeW)4", true);
     }
-    )
+
+    private static int getMaterialsId() {
+        if (startId < END_ID) {
+            return startId++;
+        }
+        throw new ArrayIndexOutOfBoundsException();
+    }
 }
