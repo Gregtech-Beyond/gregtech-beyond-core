97bdebbd3c2e5a2e8527088ef69bca2cee520091
BLocky104 <farmermick13@web.de>
2025-04-05 20:01:28 +0200

    Update GTBMaterialIconType.java

--- src/main/java/gtb/api/unification/materials/info/GTBMaterialIconType.java
+++ src/main/java/gtb/api/unification/materials/info/GTBMaterialIconType.java
@@ -9,5 +9,6 @@ public class GTBMaterialIconType {
     public static MaterialIconType itemCasing = new MaterialIconType("item_casing");
     public static MaterialIconType seedCrystal = new MaterialIconType("seed_crystal");
     public static MaterialIconType boule = new MaterialIconType("boule");
+    public static MaterialIconType concentrate = new MaterialIconType("concentrate");
     public static MaterialIconType beads = new MaterialIconType("beads");
 }
