5b9d6f672a1a00ec9dfcb384642c7b6dd642d5b2
BLocky104 <farmermick13@web.de>
2025-06-03 19:35:14 +0200

    Create MetaTileEntityTubeInTubeReactor.java

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityTubeInTubeReactor.java
@@ -0,0 +1,75 @@
+package gtb.common.metatileentities.multiblocks;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockBoilerCasing.BoilerCasingType;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+
+import gtb.api.recipes.GTBRecipeMaps;
+
+public class MetaTileEntityTubeInTubeReactor extends RecipeMapMultiblockController {
+
+    public MetaTileEntityTubeInTubeReactor(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.TUBE_IN_TUBE_REACTOR_RECIPES);
+    }
+
+    public IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("~~~~~", "F~F~F", "~~~~~", "F~F~F", "~P~P~")
+                .aisle("~~~~~", "CCCCC", "CCCCC", "CCCCC", "~P~P~")
+                .aisle("~PPP~", "CCCCC", "C~~~C", "CCSCC", "~P~P~")
+                .aisle("~~~~~", "CCCCC", "CCCCC", "CCCCC", "~~~~~")
+                .where('S', selfPredicate())
+                .where('~', any())
+                .where('C', states(getCasingState()).setMinGlobalLimited(15)
+                        .or(autoAbilities()))
+                .where('F', frames(Materials.Steel))
+                .where('P', states(MetaBlocks.BOILER_CASING.getState((BoilerCasingType.STEEL_PIPE))))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(true, true, true, true, true, true, true);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.SOLID_STEEL_CASING;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.PRIMITIVE_BLAST_FURNACE_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityTubeInTubeReactor(metaTileEntityId);
+    }
+}
