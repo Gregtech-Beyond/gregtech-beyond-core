29e22e01620257754f78faeeec50da444b050d0d
BLocky104 <farmermick13@web.de>
2025-06-03 19:31:54 +0200

    set version to 0.7-beta

--- src/main/java/gtb/api/GTBInternalTags.java
+++ src/main/java/gtb/api/GTBInternalTags.java
@@ -2,7 +2,7 @@ package gtb.api;
 
 public class GTBInternalTags {
 
-    public static final String VERSION = "0.6-beta";
+    public static final String VERSION = "0.7-beta";
     public static final String GT_VERSION_STRING = "required-after:gregtech@[2.8.8-beta,);required-after:gcym@[1.2.8-beta,);required-after:gregtechfoodoption@[1.11.1,)";
 
     private GTBInternalTags() {}
